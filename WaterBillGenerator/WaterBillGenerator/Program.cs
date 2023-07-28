08202566ba14fe4b10addc305db74c90d663d347
Bharath Nair <nairbharath007@gmail.com>
2023-07-28 21:39:39 +0530

    Added ternary operator

--- WaterBillGenerator/WaterBillGenerator/Program.cs
+++ WaterBillGenerator/WaterBillGenerator/Program.cs
@@ -15,13 +15,15 @@ namespace WaterBillGenerator
             Console.Write("Enter number of units consumed: ");
             int unitsConsumed = Convert.ToInt32(Console.ReadLine());
             //int meterCharge = 75;
-            int totalCharge;
+            /*int totalCharge;
             if(unitsConsumed<=100)
                 totalCharge = unitsConsumed * 5;
             else if(unitsConsumed<=250)
                 totalCharge = unitsConsumed * 10;    
             else
                 totalCharge = unitsConsumed * 20;
+            */
+            int totalCharge = (unitsConsumed <= 100) ? unitsConsumed * 5 : (unitsConsumed <= 250) ? unitsConsumed * 10 : unitsConsumed * 20;
 
             double totalWaterBill = totalCharge * MeterCharge;
             Console.WriteLine("Total Water Bill: " + totalWaterBill);
